f51cf1f8757c92b8b60ea284e7a6ec2ad43e1b82
medin84 <kalihanovm@gmail.com>
2015-11-28 20:47:25 +0600

    human view groovy

--- rule/USTP/Resources/scripts/page/c2/Humans.groovy
+++ rule/USTP/Resources/scripts/page/c2/Humans.groovy
@@ -13,8 +13,8 @@ class Humans extends _DoScript {
     public void doProcess(_Session session, _WebFormData formData, String lang) {
         def page = formData.getNumberValueSilently("page", 1)
 
-        def eventId = formData.getValue("event_id")
-        def formula = "form='human' and parentdocddbid='$eventId'"
+        // def eventId = formData.getValue("event_id")
+        def formula = "form='human'" // and parentdocddbid='$eventId'"
 
         def db = session.getCurrentDatabase()
         def col = db.getCollectionOfDocuments(formula, page, true, true, new SimpleDateFormat("dd.MM.yyyy"))
