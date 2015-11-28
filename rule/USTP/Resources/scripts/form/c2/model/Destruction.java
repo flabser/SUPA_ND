a96fb7816c8c4cd8f0d31fafba74dec0eecede85
medin84 <kalihanovm@gmail.com>
2015-11-28 14:58:28 +0600

    мысли в слух

---
+++ rule/USTP/Resources/scripts/form/c2/model/Destruction.java
@@ -0,0 +1,15 @@
+package form.c2.model;
+
+import java.util.List;
+
+
+public class Destruction {
+    // связь с событием через eventGUID
+
+    String guid;
+    String eventGUID;
+
+    int post_of_monitoring_warning_count;
+
+    List<PostOfMonitoringWarning> postsOfMonitoringWarning;
+}
