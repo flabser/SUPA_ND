e41df5001d7e0eb77dc5c1fe5ff55d9f73c0dd64
medin84 <kalihanovm@gmail.com>
2015-11-28 16:22:58 +0600

    geophysical view

--- rule/USTP/Resources/scripts/page/c2/Geophysical201.groovy
+++ rule/USTP/Resources/scripts/page/c2/Geophysical201.groovy
@@ -11,18 +11,13 @@ class Geophysical201 extends _DoScript {
 
     @Override
     public void doProcess(_Session session, _WebFormData formData, String lang) {
-        //	println(formData)
-        def page = 1;
-        if (formData.containsField("page") && formData.getValue("page")) {
-            page = Integer.parseInt(formData.getValue("page"))
-        }
-        def formid = formData.getValue("formid");
-        def projectid = formData.getValue("projectid")
-        def formula = "form='$formid' and project#number=$projectid";
+        def page = formData.getNumberValueSilently("page", 1)
+
+        def formula = "form='geophysical-201'"
 
         def db = session.getCurrentDatabase()
         def col = db.getCollectionOfDocuments(formula, page, true, true, new SimpleDateFormat("dd.MM.yyyy"))
-        setContent(col)
 
+        setContent(col)
     }
 }
