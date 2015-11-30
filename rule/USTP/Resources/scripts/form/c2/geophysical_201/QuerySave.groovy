6b86cbb2412d6702e22f3214325036bb130d88a0
medin84 <kalihanovm@gmail.com>
2015-11-30 12:13:55 +0600

    m

--- rule/USTP/Resources/scripts/form/c2/geophysical_201/QuerySave.groovy
+++ rule/USTP/Resources/scripts/form/c2/geophysical_201/QuerySave.groovy
@@ -13,10 +13,20 @@ class QuerySave extends _FormQuerySave {
 
         doc.addStringField("card_number", webFormData.getValue("card_number"))
         doc.addStringField("card_date", webFormData.getValue("card_date"))
+        doc.addStringField("emergency_code", webFormData.getValue("emergency_code"))
+        doc.addStringField("emergency_type", webFormData.getValue("emergency_type"))
+        doc.addStringField("emergency_date", webFormData.getValue("emergency_date"))
+        doc.addStringField("short_description", webFormData.getValue("short_description"))
+        doc.addStringField("incident_place", webFormData.getValue("incident_place"))
+        doc.addStringField("coordinates", webFormData.getValue("coordinates"))
 
         doc.setViewText(doc.getValueString("card_number"))
         doc.addViewText(doc.getValueString("card_number"))
         doc.addViewText(doc.getValueString("card_date"))
+        doc.addViewText(doc.getValueString("emergency_code"))
+        doc.addViewText(doc.getValueString("emergency_type"))
+        doc.addViewText(doc.getValueString("emergency_date"))
+        doc.addViewText(doc.getValueString("short_description"))
         //
         doc.addEditor(session.getUser().getUserID())
         doc.addEditor("[supervisor]")
