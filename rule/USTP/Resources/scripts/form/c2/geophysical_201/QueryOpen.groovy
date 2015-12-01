d0f7b776b6a6c2941ab2d419029ce08d62e8b878
medin84 <kalihanovm@gmail.com>
2015-12-01 17:38:13 +0600

    201 groovy

--- rule/USTP/Resources/scripts/form/c2/geophysical_201/QueryOpen.groovy
+++ rule/USTP/Resources/scripts/form/c2/geophysical_201/QueryOpen.groovy
@@ -1,12 +1,11 @@
 package form.c2.geophysical_201
 
-import kz.nextbase.script.*
+import kz.nextbase.script._Document
+import kz.nextbase.script._Session
+import kz.nextbase.script._WebFormData
 import kz.nextbase.script.actions._Action
 import kz.nextbase.script.actions._ActionType
-import kz.nextbase.script.constants._DocumentModeType
 import kz.nextbase.script.events._FormQueryOpen
-import kz.nextbase.script.task._Control
-import kz.pchelka.env.Environment
 
 
 class QueryOpen extends _FormQueryOpen {
@@ -38,5 +37,57 @@ class QueryOpen extends _FormQueryOpen {
 
         publishValue("card_number", doc.getValueString("card_number"))
         publishValue("card_date", doc.getValueString("card_date"))
+        publishValue("emergency_code", doc.getValueString("emergency_code"))
+        publishValue("emergency_type", doc.getValueString("emergency_type"))
+        publishValue("emergency_date", doc.getValueString("emergency_date"))
+        publishValue("short_description", doc.getValueString("short_description"))
+        //
+        def consequencesDoc = null;
+        def responses = doc.getResponses()
+        for (def rDoc : responses) {
+            if (rDoc.getDocumentForm() == 'consequences') {
+                consequencesDoc = rDoc;
+                break;
+            }
+        }
+
+        if (consequencesDoc) {
+            publishValue("human_in_zone_emergency_count", consequencesDoc.getValueNumber("human_in_zone_emergency_count"))
+
+            publishValue("affected_count", consequencesDoc.getValueNumber("affected_count"))
+            publishValue("affected_children_count", consequencesDoc.getValueNumber("affected_children_count"))
+            publishValue("affected_personnel_k4s_count", consequencesDoc.getValueNumber("affected_personnel_k4s_count"))
+
+            publishValue("dead_count", consequencesDoc.getValueNumber("dead_count"))
+            publishValue("dead_children_count", consequencesDoc.getValueNumber("dead_children_count"))
+            publishValue("dead_personnel_k4s_count", consequencesDoc.getValueNumber("dead_personnel_k4s_count"))
+
+            publishValue("rescued_count", consequencesDoc.getValueNumber("rescued_count"))
+            publishValue("rescued_children_count", consequencesDoc.getValueNumber("rescued_children_count"))
+
+            publishValue("missing_count", consequencesDoc.getValueNumber("missing_count"))
+            publishValue("missing_children_count", consequencesDoc.getValueNumber("missing_children_count"))
+            publishValue("missing_personnel_k4s_count", consequencesDoc.getValueNumber("missing_personnel_k4s_count"))
+
+            publishValue("searchrescue_found_count", consequencesDoc.getValueNumber("searchrescue_found_count"))
+            publishValue("searchrescue_taken_med_count", consequencesDoc.getValueNumber("searchrescue_taken_med_count"))
+            publishValue("searchrescue_taken_med_k4s_count", consequencesDoc.getValueNumber("searchrescue_taken_med_k4s_count"))
+
+            publishValue("requiring_evacuation_count", consequencesDoc.getValueNumber("requiring_evacuation_count"))
+            publishValue("evacuees_count", consequencesDoc.getValueNumber("evacuees_count"))
+            publishValue("evacuees_children_count", consequencesDoc.getValueNumber("evacuees_children_count"))
+
+            publishValue("first_aid_count", consequencesDoc.getValueNumber("first_aid_count"))
+            publishValue("first_aid_children_count", consequencesDoc.getValueNumber("first_aid_children_count"))
+
+            publishValue("hospitalized_count", consequencesDoc.getValueNumber("hospitalized_count"))
+            publishValue("hospitalized_children_count", consequencesDoc.getValueNumber("hospitalized_children_count"))
+
+            publishValue("homeless_count", consequencesDoc.getValueNumber("homeless_count"))
+            publishValue("homeless_children_count", consequencesDoc.getValueNumber("homeless_children_count"))
+
+            publishValue("dead_domestic_animal_count", consequencesDoc.getValueNumber("dead_domestic_animal_count"))
+            publishValue("evacuees_domestic_animal_count", consequencesDoc.getValueNumber("evacuees_domestic_animal_count"))
+        }
     }
 }
--- rule/USTP/Resources/scripts/form/c2/geophysical_201/QuerySave.groovy
+++ rule/USTP/Resources/scripts/form/c2/geophysical_201/QuerySave.groovy
@@ -39,46 +39,48 @@ public class QuerySave extends _FormQuerySave {
     }
 
     def addConsequencesDoc(_Session session, _Document doc, _WebFormData webFormData) {
-        def consequencesDoc;
+        def consequencesDoc = null;
         if (doc.isNewDoc) {
             consequencesDoc = new _Document(session.getCurrentDatabase());
+            consequencesDoc.setForm("consequences");
+            consequencesDoc.setParentDoc(doc);
         } else {
-            String eventId = doc.getID()
-            def viewParam = session.createViewEntryCollectionParam();
-            viewParam.setQuery("form = 'human' & parentdocddbid = '" + eventId + "'")
-                    .setPageNum(0)
-                    .setPageSize(1)
-                    .setCheckResponse(false);
-
-            def col = session.getCurrentDatabase().getCollectionOfDocuments(viewParam);
-            consequencesDoc = col.getEntries().iterator().next().getDocument();
+            def responses = doc.getResponses()
+            for (def rDoc : responses) {
+                if (rDoc.getDocumentForm() == 'consequences') {
+                    consequencesDoc = rDoc;
+                    break;
+                }
+            }
+            if (consequencesDoc == null) {
+                consequencesDoc = new _Document(session.getCurrentDatabase());
+                consequencesDoc.setForm("consequences");
+                consequencesDoc.setParentDoc(doc);
+            }
         }
-
-        consequencesDoc.setForm("consequences");
-        consequencesDoc.setParentDoc(doc);
 
         consequencesDoc.addNumberField("human_in_zone_emergency_count", webFormData.getNumberValueSilently("human_in_zone_emergency_count", 0));
 
         consequencesDoc.addNumberField("affected_count", webFormData.getNumberValueSilently("affected_count", 0));
         consequencesDoc.addNumberField("affected_children_count", webFormData.getNumberValueSilently("affected_children_count", 0));
-        consequencesDoc.addNumberField("affected_personnel_k4s_mvd_rk_count", webFormData.getNumberValueSilently("affected_personnel_k4s_mvd_rk_count", 0));
+        consequencesDoc.addNumberField("affected_personnel_k4s_count", webFormData.getNumberValueSilently("affected_personnel_k4s_count", 0));
 
         consequencesDoc.addNumberField("dead_count", webFormData.getNumberValueSilently("dead_count", 0));
         consequencesDoc.addNumberField("dead_children_count", webFormData.getNumberValueSilently("dead_children_count", 0));
-        consequencesDoc.addNumberField("dead_personnel_k4s_mvd_rk_count", webFormData.getNumberValueSilently("dead_personnel_k4s_mvd_rk_count", 0));
+        consequencesDoc.addNumberField("dead_personnel_k4s_count", webFormData.getNumberValueSilently("dead_personnel_k4s_count", 0));
 
         consequencesDoc.addNumberField("rescued_count", webFormData.getNumberValueSilently("rescued_count", 0));
         consequencesDoc.addNumberField("rescued_children_count", webFormData.getNumberValueSilently("rescued_children_count", 0));
 
         consequencesDoc.addNumberField("missing_count", webFormData.getNumberValueSilently("missing_count", 0));
         consequencesDoc.addNumberField("missing_children_count", webFormData.getNumberValueSilently("missing_children_count", 0));
-        consequencesDoc.addNumberField("missing_personnel_k4s_mvd_rk_count", webFormData.getNumberValueSilently("missing_personnel_k4s_mvd_rk_count", 0));
+        consequencesDoc.addNumberField("missing_personnel_k4s_count", webFormData.getNumberValueSilently("missing_personnel_k4s_count", 0));
 
-        consequencesDoc.addNumberField("search_rescue_found_count", webFormData.getNumberValueSilently("search_rescue_found_count", 0));
-        consequencesDoc.addNumberField("search_rescue_taken_medical_count", webFormData.getNumberValueSilently("search_rescue_taken_medical_count", 0));
-        consequencesDoc.addNumberField("search_rescue_taken_medical_k4s_mvd_rk_count", webFormData.getNumberValueSilently("search_rescue_taken_medical_k4s_mvd_rk_count", 0));
+        consequencesDoc.addNumberField("searchrescue_found_count", webFormData.getNumberValueSilently("searchrescue_found_count", 0));
+        consequencesDoc.addNumberField("searchrescue_taken_med_count", webFormData.getNumberValueSilently("searchrescue_taken_med_count", 0));
+        consequencesDoc.addNumberField("searchrescue_taken_med_k4s_count", webFormData.getNumberValueSilently("searchrescue_taken_med_k4s_count", 0));
 
-        consequencesDoc.addNumberField("requiring_evacuation_people_count", webFormData.getNumberValueSilently("requiring_evacuation_people_count", 0));
+        consequencesDoc.addNumberField("requiring_evacuation_count", webFormData.getNumberValueSilently("requiring_evacuation_count", 0));
         consequencesDoc.addNumberField("evacuees_count", webFormData.getNumberValueSilently("evacuees_count", 0));
         consequencesDoc.addNumberField("evacuees_children_count", webFormData.getNumberValueSilently("evacuees_children_count", 0));
 
