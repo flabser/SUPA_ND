3c14e7131fded0fc549c0994afd585a70ceb886c
medin84 <kalihanovm@gmail.com>
2015-12-02 19:47:13 +0600

    op plans & work

---
+++ rule/USTP/Resources/scripts/ustp/modules/operational_plans_work/form/suggestion/QueryOpen.groovy
@@ -0,0 +1,43 @@
+package ustp.modules.operational_plans_work.form.suggestion
+
+import kz.nextbase.script._Document
+import kz.nextbase.script._Session
+import kz.nextbase.script._WebFormData
+import kz.nextbase.script.actions._Action
+import kz.nextbase.script.actions._ActionType
+import kz.nextbase.script.events._FormQueryOpen
+
+
+class QueryOpen extends _FormQueryOpen {
+
+    @Override
+    public void doQueryOpen(_Session session, _WebFormData webFormData, String lang) {
+        publishValue("title", getLocalizedWord("Новое предложение", lang))
+
+        def nav = session.getPage("outline", webFormData)
+        publishElement(nav)
+
+        def actionBar = session.createActionBar();
+        actionBar.addAction(new _Action(getLocalizedWord("Сохранить и закрыть", lang), "", _ActionType.SAVE_AND_CLOSE))
+        actionBar.addAction(new _Action(getLocalizedWord("Закрыть", lang), "", _ActionType.CLOSE))
+        publishElement(actionBar)
+    }
+
+    @Override
+    public void doQueryOpen(_Session session, _Document doc, _WebFormData webFormData, String lang) {
+        publishValue("title", getLocalizedWord("Предложение", lang))
+
+        def nav = session.getPage("outline", webFormData)
+        publishElement(nav)
+
+        def actionBar = session.createActionBar();
+        actionBar.addAction(new _Action(getLocalizedWord("Сохранить и закрыть", lang), "", _ActionType.SAVE_AND_CLOSE))
+        actionBar.addAction(new _Action(getLocalizedWord("Закрыть", lang), "", _ActionType.CLOSE))
+        publishElement(actionBar)
+
+        publishValue("name", doc.getValueString("name"))
+        publishValue("address", doc.getValueString("address"))
+        publishValue("phone", doc.getValueString("phone"))
+        publishValue("details", doc.getValueString("details"))
+    }
+}
