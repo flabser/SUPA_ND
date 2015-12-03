519a77ab35e2aca6f925087cf26e150b3c4f74a1
medin84 <kalihanovm@gmail.com>
2015-12-03 17:01:35 +0600

    op

--- rule/OpPlansWork/Resources/scripts/ustp/modules/operational_plans_work/page/Assignees.groovy
+++ rule/OpPlansWork/Resources/scripts/ustp/modules/operational_plans_work/page/Assignees.groovy
@@ -2,13 +2,7 @@ package ustp.modules.operational_plans_work.page
 
 import kz.nextbase.script._Session
 import kz.nextbase.script._WebFormData
-import kz.nextbase.script.actions._Action
-import kz.nextbase.script.actions._ActionBar
-import kz.nextbase.script.actions._ActionType
 import kz.nextbase.script.events._DoScript
-
-import java.text.SimpleDateFormat
-
 
 class Assignees extends _DoScript {
 
